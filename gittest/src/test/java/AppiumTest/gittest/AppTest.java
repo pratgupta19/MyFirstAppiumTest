23a05da4c396152ef95a6e7afa4f4896eaae2a3e
pratik.gupta <pratik.gupta@PratikGupta.igglobal.com>
2016-05-09 18:03:31 +0530

    # WARNING: head commit changed in the meantime
    Pratik GIT AppiumTest

---
+++ gittest/src/test/java/AppiumTest/gittest/AppTest.java
@@ -0,0 +1,35 @@
+package AppiumTest.gittest;
+import io.appium.java_client.android.AndroidDriver;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+public class AppTest {
+		@Test
+		public void StartTest() throws MalformedURLException, InterruptedException {
+			AndroidDriver driver;	
+			File classpathRoot = new File(System.getProperty("user.dir"));
+			File appDir = new File(classpathRoot, "/App/");
+			File app = new File(appDir, "AmazonIndiaShopping_v6.4.0.300.apk");
+		
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			//capabilities.setCapability(CapabilityType.BROWSER_NAME, ""); 
+			capabilities.setCapability("deviceName", "Test_Device");
+			capabilities.setCapability("platformVersion", "6.0");
+			capabilities.setCapability("platformName", "Android");
+			capabilities.setCapability("app", app.getAbsolutePath());
+			capabilities.setCapability("appPackage", "in.amazon.mShop.android.shopping");
+			capabilities.setCapability("appActivity", "com.amazon.mShop.home.HomeActivity");
+
+			driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+			driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+		//	Thread.sleep(10000);
+			driver.quit();
+	
+	}
+
+}
